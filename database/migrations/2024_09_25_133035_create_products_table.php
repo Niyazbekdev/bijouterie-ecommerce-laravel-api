1b3bc87a2ea232dc1afc95c52d2557b150cec5d2
Niyazbekdev <niyazbekk001@gmail.com>
2024-10-01 09:51:40 +0500

    update column name string to text

--- database/migrations/2024_09_25_133035_create_products_table.php
+++ database/migrations/2024_09_25_133035_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('name');
             $table->text('description')->nullable();
             $table->unsignedBigInteger('price');
             $table->unsignedInteger('quantity');
